6875c1c6e967593d19cfbceee3782a013867a91c
Rennan Oliveira <rennan_neves@live.com>
2021-03-16 16:48:25 -0300

    Category Find By Name

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,4 @@
-import { response, Router } from "express";
+import { Router } from "express";
 
 import { CategoryRepository } from "../repositories/CategoriesRepositories";
 
@@ -7,6 +7,12 @@ const categoryRepository = new CategoryRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
+
+  const categoryAlreadyExists = categoryRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category already exists!" });
+  }
 
   categoryRepository.create({ name, description });
 
