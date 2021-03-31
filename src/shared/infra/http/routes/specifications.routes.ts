c88bca852c5344980fcae946f27db901ac8b11f4
Rennan Oliveira <rennan_neves@live.com>
2021-03-31 18:11:28 -0300

    Ensure Admin Middleware && fix routes

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { ListSpesficationsController } from "@modules/cars/useCases/listSpecifications/ListSpecificationsController";
+import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const specificationsRoutes = Router();
@@ -9,9 +10,12 @@ const specificationsRoutes = Router();
 const createSpecificationController = new CreateSpecificationController();
 const listSpesficationsController = new ListSpesficationsController();
 
-specificationsRoutes.use(ensureAuthenticated);
-
-specificationsRoutes.post("/", createSpecificationController.handlle);
+specificationsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationController.handlle
+);
 
 specificationsRoutes.get("/", listSpesficationsController.handdle);
 
